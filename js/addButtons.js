658fbbc8eb65aa10c820683cd7d5425209fd36c4
Mayank300 <mratre300@gmail.com>
2022-04-05 15:58:59 +0530

    commit2

--- js/addButtons.js
+++ js/addButtons.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("create-buttons", {
-  init: function() {
+  init: function () {
     // 1. Create the button
-    var button1 = document.createElement("button");    
+    var button1 = document.createElement("button");
     button1.innerHTML = "RATE DISH";
     button1.setAttribute("id", "rating-button");
     button1.setAttribute("class", "btn btn-warning mr-3");
@@ -12,9 +12,9 @@ AFRAME.registerComponent("create-buttons", {
     button2.setAttribute("id", "order-button");
     button2.setAttribute("class", "btn btn-warning mr-4");
 
-    // 2. Append somewhere
+    // 3. Append somewhere
     var buttonDiv = document.getElementById("button-div");
     buttonDiv.appendChild(button1);
     buttonDiv.appendChild(button2);
-  }
+  },
 });
--- js/handlemarker.js
+++ js/handlemarker.js
@@ -1,9 +1,79 @@
+var table_number = null;
+
 AFRAME.registerComponent("handleMarker", {
   init: async function () {
+    if (table_number === null) {
+      this.askTableNumber();
+    }
     var dishes = await this.getDishes();
     this.el.addEventListener("markerFound", () => {
-      var markerId = this.el.id;
-      this.handleMarkerFound(dishes, markerId);
+      if (table_number !== null) {
+        var markerId = this.el.id;
+        this.handleMarkerFound(dishes, markerId);
+      }
+    });
+
+    this.el.addEventListener("markerLost", () => {
+      this.handleMarkerLost();
+    });
+  },
+
+  getDishes: async function () {
+    return await firebase
+      .firestore()
+      .collection("menu")
+      .get()
+      .then((data) => {
+        return data.docs.map((doc) => {
+          doc.data();
+        });
+      });
+  },
+
+  handleOrder: function (t_num, dish) {
+    database
+      .firestore()
+      .collection("tables")
+      .doc(t_num)
+      .get()
+      .then((doc) => {
+        var details = doc.data();
+        var parent = details["current_orders"][dish.id];
+        if (parent) {
+          parent["quantity"] += 1;
+          var currentQuantity = parent["quantity"];
+          details["current_orders"][dish.id]["subtotal"] =
+            dish.price * currentQuantity;
+        } else {
+          parent = {
+            item: dish.dish_name,
+            price: dish.price,
+            quantity: 1,
+            subtotal: dish.price * 1,
+          };
+        }
+        details.total_bill += dish.price;
+        firebase.firestore().collection("tables").doc(doc.id).update(parent);
+      });
+  },
+
+  askTableNumber: async function () {
+    var iconUrl =
+      "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/hunger.png";
+    swal({
+      title: "Welcome to Crown Plaza",
+      icon: iconUrl,
+      content: {
+        element: "input",
+        attributes: {
+          placeholder: "Enter your table number",
+          type: number,
+          min: 1,
+        },
+      },
+      closeOnClickOutside: false,
+    }).then((value) => {
+      table_number = value;
     });
   },
 
@@ -32,6 +102,7 @@ AFRAME.registerComponent("handleMarker", {
         text: "Have a nice day",
       });
     });
+
     orderButton.addEventListener("click", () => {
       swal({
         icon: "success",
@@ -48,5 +119,10 @@ AFRAME.registerComponent("handleMarker", {
     model.setAttribute("position", dish.model_geometry.position);
     model.setAttribute("rotation", dish.model_geometry.rotation);
     model.setAttribute("scale", dish.model_geometry.scale);
+  },
+
+  handleMarkerLost: function () {
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "null";
   },
 });
